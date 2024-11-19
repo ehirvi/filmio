289dcb732e819745022a2c6927fda8b7dc07785b
ehirvi <eino.hirvi@gmail.com>
2024-11-19 13:02:36 +0200

    Modified movie swiper to fetch next batch of movies in the background just before the previous queue is empty to avoid visible lag effect

--- screens/HomeScreen/MovieSwiper.tsx
+++ screens/HomeScreen/MovieSwiper.tsx
@@ -6,7 +6,7 @@ import {
 import { useEffect, useState } from "react";
 import movieService from "../../services/movieService";
 import MovieCard from "../../components/MovieCard";
-import { FAB } from "react-native-paper";
+import { ActivityIndicator, FAB, MD2Colors } from "react-native-paper";
 import { StyleSheet, View } from "react-native";
 import { useSQLiteContext } from "expo-sqlite";
 import useMovieStore from "../../hooks/useStore";
@@ -15,26 +15,28 @@ type Props = NativeStackScreenProps<HomeScreenStackParamlist, "MovieSwiper">;
 
 const MovieSwiper = ({ route }: Props) => {
   const { genres } = route.params;
-  const [movies, setMovies] = useState<DiscoverMovieResponse["results"]>();
+  const [movies, setMovies] = useState<DiscoverMovieResponse["results"]>([]);
   const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(true);
   const db = useSQLiteContext();
   const getSavedMovies = useMovieStore((state) => state.getSavedMovies);
   const saveMovie = useMovieStore((state) => state.saveMovie);
 
   useEffect(() => {
     const fetchMovies = async () => {
+      setLoading(true);
       const results = await movieService.getMoviesByGenre(genres, page);
-      setMovies(results);
+      setMovies((m) => m.concat(results));
+      setLoading(false);
     };
     fetchMovies();
   }, [page]);
 
   const removeFromQueue = (movies: DiscoverMovieResponse["results"]) => {
-    if (movies.length === 1) {
+    if (movies.length === 2) {
       setPage((page) => page + 1);
-    } else {
-      setMovies(movies.slice(1));
     }
+    setMovies(movies.slice(1));
   };
 
   const handleAccept = (movies: DiscoverMovieResponse["results"]) => {
@@ -43,28 +45,42 @@ const MovieSwiper = ({ route }: Props) => {
     removeFromQueue(movies);
   };
 
-  if (movies) {
-    return (
-      <View>
-        <MovieCard movie={movies[0]} />
-        <View style={styles.buttonRow}>
-          <FAB
-            icon="close-thick"
-            size="large"
-            onPress={() => removeFromQueue(movies)}
-          />
-          <FAB
-            icon="heart"
-            size="large"
-            onPress={() => void handleAccept(movies)}
+  return (
+    <>
+      {loading ? (
+        <View style={styles.loadingIndicator}>
+          <ActivityIndicator
+            animating={true}
+            color={MD2Colors.red800}
+            size={"large"}
           />
         </View>
-      </View>
-    );
-  }
+      ) : (
+        <View>
+          <MovieCard movie={movies[0]} />
+          <View style={styles.buttonRow}>
+            <FAB
+              icon="close-thick"
+              size="large"
+              onPress={() => removeFromQueue(movies)}
+            />
+            <FAB
+              icon="heart"
+              size="large"
+              onPress={() => void handleAccept(movies)}
+            />
+          </View>
+        </View>
+      )}
+    </>
+  );
 };
 
 const styles = StyleSheet.create({
+  loadingIndicator: {
+    flex: 1,
+    justifyContent: "center",
+  },
   buttonRow: {
     flexDirection: "row",
     justifyContent: "space-around",
