a1240925f4e7c747f3602c8d25f1321b97a2d406
ehirvi <eino.hirvi@gmail.com>
2024-11-20 23:51:32 +0200

    Add modal that opens when clicked on a movie card and shows movie description

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,28 +1,53 @@
-import { Card, Surface } from "react-native-paper";
-import { Image, StyleSheet } from "react-native";
+import { Card, Modal, Portal, Surface } from "react-native-paper";
+import { Image, Pressable, StyleSheet, Text, View } from "react-native";
 import { POSTER_URL } from "../utils/constants";
 import { DiscoverMovieResponse } from "../utils/types";
 import { parseReleaseYear } from "../utils/helpers";
+import { useMemo, useState } from "react";
 
 interface Props {
   movie: DiscoverMovieResponse["results"][number];
 }
 
 const MovieCard = ({ movie }: Props) => {
-  const releaseYear = parseReleaseYear(movie.release_date);
+  const [modalOpen, setModalOpen] = useState(false);
+  const releaseYear = useMemo(
+    () => parseReleaseYear(movie.release_date),
+    [movie.id]
+  );
+
+  const openModal = () => setModalOpen(true);
+  const closeModal = () => setModalOpen(false);
 
   return (
-    <Card style={styles.card}>
-      <Card.Content>
-        <Surface elevation={3}>
-          <Image
-            source={{ uri: `${POSTER_URL}/${movie.poster_path}` }}
-            style={styles.image}
-          />
-        </Surface>
-      </Card.Content>
-      <Card.Title title={`${movie.title} (${releaseYear})`} />
-    </Card>
+    <View>
+      {modalOpen && (
+        <Portal>
+          <Modal
+            contentContainerStyle={{ margin: 50 }}
+            visible={modalOpen}
+            onDismiss={closeModal}
+          >
+            <Surface style={styles.modalContent} elevation={5}>
+              <Text>{movie.overview}</Text>
+            </Surface>
+          </Modal>
+        </Portal>
+      )}
+      <Pressable onPress={openModal}>
+        <Card style={styles.card}>
+          <Card.Content>
+            <Surface elevation={3}>
+              <Image
+                source={{ uri: `${POSTER_URL}/${movie.poster_path}` }}
+                style={styles.image}
+              />
+            </Surface>
+          </Card.Content>
+          <Card.Title title={`${movie.title} (${releaseYear})`} />
+        </Card>
+      </Pressable>
+    </View>
   );
 };
 
@@ -36,6 +61,10 @@ const styles = StyleSheet.create({
     width: 270,
     height: "auto",
     aspectRatio: 2 / 3,
+  },
+  modalContent: {
+    padding: 50,
+    borderRadius: 10,
   },
 });
 
