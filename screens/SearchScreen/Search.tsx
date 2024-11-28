6c8e1097c2028b39a61179230c32523a1f17f1cb
ehirvi <eino.hirvi@gmail.com>
2024-11-28 16:05:42 +0200

    Fix search results to be sorted by popularity/votes instead of random

--- screens/SearchScreen/Search.tsx
+++ screens/SearchScreen/Search.tsx
@@ -1,23 +1,37 @@
 import { useEffect, useRef, useState } from "react";
 import { View } from "react-native";
 import { Searchbar } from "react-native-paper";
-import { Movie, MovieSearchResult } from "../../utils/types";
+import { MovieSearchResult } from "../../utils/types";
 import searchService from "../../services/searchService";
 import MovieList from "../../components/MovieList";
 
 const Search = () => {
   const [textInput, setTextInput] = useState("");
   const page = useRef(1);
-  const [searchResults, setSearchResults] = useState<MovieSearchResult>();
+  const [searchResultMovies, setSearchResultMovies] =
+    useState<MovieSearchResult["results"]>();
+  const [searchResultPages, setSearchResultPages] =
+    useState<MovieSearchResult["total_pages"]>();
 
   useEffect(() => {
-    const timeoutId = setTimeout(() => void fetchResults(textInput), 1000);
+    const timeoutId = setTimeout(() => {
+      if (textInput.length === 0) {
+        setSearchResultMovies(undefined);
+        setSearchResultPages(undefined);
+      } else {
+        void fetchResults(textInput);
+      }
+    }, 1000);
     return () => clearTimeout(timeoutId);
   }, [textInput]);
 
   const fetchResults = async (query: string) => {
     const res = await searchService.getResultPage(query, page.current);
-    setSearchResults(res);
+    const sortedMovies = [...res.results].sort(
+      (a, b) => b.vote_count - a.vote_count
+    );
+    setSearchResultMovies(sortedMovies);
+    setSearchResultPages(res.total_pages);
   };
 
   const handleSearch = (text: string) => {
@@ -34,7 +48,9 @@ const Search = () => {
         value={textInput}
         onChangeText={(text) => handleSearch(text)}
       />
-      {searchResults && <MovieList movies={searchResults.results} />}
+      {searchResultMovies && searchResultPages && searchResultPages > 0 && (
+        <MovieList movies={searchResultMovies} />
+      )}
     </View>
   );
 };
